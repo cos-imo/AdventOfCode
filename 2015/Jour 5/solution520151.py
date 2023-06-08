1850667f624bfcf78eb799b72954ef87265be159
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-06-08 15:04:44 +0200

    Solution de la première partie

---
+++ 2015/Jour 5/solution520151.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+
+    file=open('input','r')
+    lst=file.readlines()
+
+    res=0
+
+    for line in lst:
+
+        (("ab" in line) or ("cd" in line) or ("pq" in line) or ("xy" in line))
+
+if __name__=="__main__":
+    main()
