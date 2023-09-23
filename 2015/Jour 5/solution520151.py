f8fffb9059c7e55d63e3a1cb7ec3e8cd22630550
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-09-23 20:03:06 +0200

    Solution de la première partie

--- 2015/Jour 5/solution520151.py
+++ 2015/Jour 5/solution520151.py
@@ -1,4 +1,4 @@
-import sys
+import sys, string, copy
 
 def main():
 
@@ -8,8 +8,15 @@ def main():
     res=0
 
     for line in lst:
-
-        (("ab" in line) or ("cd" in line) or ("pq" in line) or ("xy" in line))
+        vowels="aeiou"
+        double=0
+        vowels_in_word=0
+        for i in range(len(line)-1):
+            double+=(line[i+1]==line[i])
+            vowels_in_word+=(line[i] in vowels)
+        vowels_in_word+=(line[-1] in vowels)
+        res+=((vowels_in_word>2) and (not (("ab" in line) or ("cd" in line) or ("pq" in line) or ("xy" in line)) and double!=0))
+    sys.stdout.write("{} strings are nice".format(str(res)))
 
 if __name__=="__main__":
     main()
