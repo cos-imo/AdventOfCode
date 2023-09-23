1a5e669d70250b99cdc5f4729208f728b21e06a3
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-09-23 19:23:31 +0200

    Solution de la première partie

--- 2015/Jour 4/solution420151.py
+++ 2015/Jour 4/solution420151.py
@@ -1,19 +1,16 @@
-import hashlib
- 
-
- 
-import sys
+import hashlib, sys
 
 def main():
 
     file=open('input','r')
     string=file.readlines()[0]
     
+    proof_of_work=0
 
-    while (hashlib.md5(string+).digest()[0:5]!="00000"):
-
+    while (hashlib.md5((string+str(proof_of_work)).encode()).hexdigest()[0:5]!="00000"):
+        proof_of_work+=1
             
-    sys.stdout.write("Total needed: " + str(res))
+    sys.stdout.write("proof of work: " + str(proof_of_work))
 
 if __name__=="__main__":
     main()
