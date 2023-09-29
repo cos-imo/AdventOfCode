71aca8e2308be172b32d63c86bd95b1db2ef499d
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-09-30 01:09:05 +0200

    Solution de la première partie

---
+++ 2022/Jour 6/solution620221.py
@@ -0,0 +1,21 @@
+import sys
+
+def check(lst):
+    for i in range(0,len(lst)-1):
+        if lst[i] in (lst[:i]+lst[(i+1):]):
+            return False
+    return True
+
+def main():
+    file=open('input','r')
+    lst=file.readlines()
+
+    for i in range(4,len(lst[0])-1):
+        if check(lst[0][i-4:i]):
+            print(i)
+            break
+
+    return "Erreur"
+
+if __name__=="__main__":
+    main()
